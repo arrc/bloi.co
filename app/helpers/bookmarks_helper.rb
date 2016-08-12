5b1fc06b561f67dd5f74a0a36d1870d3ec4b437c
arrC <naveen@arrc.in>
2016-08-12 21:45:52 +0530

    finished styling the topic module

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,6 +1,17 @@
 module BookmarksHelper
 
   def set_url
-    
+
   end
+
+  def flag_color(flag)
+    if flag == 1 # important
+      flag_color = "bl_bookmarks__item--flag__important ion-record"
+    elsif flag == 2 # very important
+      flag_color = "bl_bookmarks__item--flag__very-important ion-record"
+    elsif flag == 3 # normal
+      flag_color = "bl_bookmarks__item--flag__normal ion-record"
+    end
+  end
+
 end
